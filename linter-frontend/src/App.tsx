cd6c7d118756fe27e530b14674550e15cd282985
Prx1i1 <pawlo.augustyn@gmail.com>
2026-03-18 14:33:01 +0100

    base

--- linter-frontend/src/App.tsx
+++ linter-frontend/src/App.tsx
@@ -1,11 +1,29 @@
-import { useState } from "react";
+import { ChangeEventHandler, useState } from "react";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import { Button, Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
+ 
+// Define the form values type
+type FormValues = {
+  file: FileList | null;
+};
+
+
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [greetMsg, setGreetMsg] = useState<string>("");
+  const [name, setName] = useState<string>("");
+  const { register, handleSubmit } = useForm<FormValues>();
+ 
+  //if yes, dark theme
+  const [theme, setTheme] = useState<boolean>(true);
+
+  const onSubmit = (data: FormValues) => {
+    //send file to server
+    console.log(data.file);
+  };
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
@@ -13,24 +31,52 @@ function App() {
   }
 
   return (
-    <main className="container">
+    <>
+      <header>
+        <Navbar expand="lg" className="bg-body-tertiary">
+          <Container className="py-0">
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                  <Nav.Link href="#home">Home</Nav.Link>
+                  <Nav.Link href="#link">Link</Nav.Link>
+                  <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">
+                      Another action
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.4">
+                      Separated link
+                    </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
+      <main>
+        <div className="d-flex flex-row lg-container w-100">
+          <Container className="left">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <input type="file" {...register('file')} />
+              <Button type="submit">Submit</Button>
+            </form>
+            <Container className="filePreview text-left">
+              placeholder text
+            </Container>
+          </Container>
+          <Container className="right">
 
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
-    </main>
+          </Container>
+        </div>
+      </main>
+      <footer>
+        foot
+      </footer>
+    </>
   );
 }
 
